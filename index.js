75ff9210d07400d0e511a168ec065cd686cdd38a
ultramarilia <ultramarilia@hotmail.com>
2021-05-28 08:57:18 -0300

    adicionando put e delete

--- index.js
+++ index.js
@@ -13,9 +13,45 @@ const usuarioFernando = {
 const usuarios = [usuarioFernando];
 
 app.get('/usuarios', (req, res) => {
-    res.json(usuarios);
+    res.json(usuarios)
     //res.send(JSON.stringify(usuarios))
 });
+
+app.post('/usuarios', (req, res) => {
+    const {nome, idade} = req.body;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    };
+
+    usuarios.push(novoUsuario);
+
+    res.json(novoUsuario);
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id));
+    usuario.nome = nome;
+    usuario.idade = idade;
+
+    res.json(usuario);
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id));
+    
+    usuarios.splice(index, 10);
+
+    res.status(204).send();
+})
 
 app.listen(port, () => {
     console.log('Server is up and runnin on port ' + port)
